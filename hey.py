d67921c0916982671fda3f20c6a231592439b5a1
Prabhav <pkanneganti08@gmail.com>
2025-05-17 22:25:14 -0400

    Add files via upload

---
+++ hey.py
@@ -0,0 +1,8 @@
+print("Hello")
+print("How are you doing?")
+password = input("Enter an password")
+password_real = "diamond"
+if password = password_real:
+    print("Correct!")
+else:
+    print("Try again")
